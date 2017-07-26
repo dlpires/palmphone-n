d76853a551aa97565824eb39ecd5dddd7971bf06
dlpires <dlpires@outlook.com>
2017-07-26 20:27:51 -0300

    Primeira Página Criada

---
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/login_page.java
@@ -0,0 +1,16 @@
+package com.appnative.dlpires.palmphone_n;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dlpires on 26/07/17.
+ */
+
+public class login_page extends Activity{
+    @Override
+    public void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.login_page);
+    }
+}
