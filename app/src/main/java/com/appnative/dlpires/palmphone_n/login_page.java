55979dade5127a1ed17853584952c80e8ee22433
dlpires <dlpires@outlook.com>
2017-07-27 21:51:11 -0300

    v0.2 - Com tela de Menu

--- app/src/main/java/com/appnative/dlpires/palmphone_n/login_page.java
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/login_page.java
@@ -1,7 +1,9 @@
 package com.appnative.dlpires.palmphone_n;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by dlpires on 26/07/17.
@@ -9,8 +11,13 @@ import android.os.Bundle;
 
 public class login_page extends Activity{
     @Override
-    public void onCreate(Bundle b){
+    protected void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.login_page);
+    }
+
+    public void verificaLogin(View v){
+        Intent i = new Intent(this, menu_page.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/menu_page.java
@@ -0,0 +1,16 @@
+package com.appnative.dlpires.palmphone_n;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dlpires on 27/07/17.
+ */
+
+public class menu_page extends Activity {
+    @Override
+    protected void onCreate(Bundle b){
+        super.onCreate(b);
+        setContentView(R.layout.menu_page);
+    }
+}
