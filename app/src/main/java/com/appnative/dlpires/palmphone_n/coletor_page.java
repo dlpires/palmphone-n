7d8553fa3c0da00dfb81f35b763f32f84d5bee64
dlpires <dlpires@outlook.com>
2017-07-29 22:21:18 -0300

    Todas as telas pré prontas

--- app/src/main/java/com/appnative/dlpires/palmphone_n/coletor_page.java
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/coletor_page.java
@@ -1,7 +1,9 @@
 package com.appnative.dlpires.palmphone_n;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by dlpires on 28/07/17.
@@ -12,5 +14,10 @@ public class coletor_page extends Activity {
     protected void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.coletor_page);
+    }
+
+    public void pageColeta(View v){
+        Intent i = new Intent(this, coletadados_page.class);
+        startActivity(i);
     }
 }
