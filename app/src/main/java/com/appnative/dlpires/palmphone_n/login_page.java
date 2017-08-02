7a2d154ba962f239c6be2f6b7c07516808ddee61
dlpires <dlpires@outlook.com>
2017-08-01 23:20:38 -0300

    Telas ajustadas e verificacao de login iniciada

--- app/src/main/java/com/appnative/dlpires/palmphone_n/login_page.java
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/login_page.java
@@ -5,15 +5,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 /**
  * Created by dlpires on 26/07/17.
  */
 
 public class login_page extends Activity{
+
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
     @Override
     protected void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.login_page);
+        auth = FirebaseAuth.getInstance();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
 
     public void verificaLogin(View v){
