54500d9a18e83d0113ac6fb6e02f131f67f65f07
dlpires <dlpires@outlook.com>
2018-01-14 13:25:36 -0200

    Mudança de Servidor Firebase

--- app/src/main/java/com/appnative/dlpires/palmphone_n/ColetorDadosPage.java
+++ app/src/main/java/com/appnative/dlpires/palmphone_n/ColetorDadosPage.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
